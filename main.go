8a249ba24fd15e66a91f5f38f76026d06d77c2cd
Antwaan Harrison <antwaanvh@gmail.com>
2021-06-28 21:30:20 -0400

    task: add global queue + repo

--- main.go
+++ main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
+	"container/list"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"regexp"
+	"strconv"
 	"strings"
+	"time"
 )
+
+type PasswordRow struct {
+	id        int
+	value     string
+	createdAt int64
+}
+
+type Stats struct {
+	total   int
+	average int
+}
+
+var queue = list.New()
+var repo = map[int]string{} // lock and write
+
+// watch queue
+func queueWatcher(q *list.List) {
+	for e := q.Front(); e != nil; e = e.Next() {
+		fmt.Println("Watching...", e)
+	}
+}
 
 func main() {
 	// import environment vars
 	setEnvFromFile(".env")
+
+	go queueWatcher(queue)
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/hash", PostHash)
@@ -26,7 +54,19 @@ func PostHash(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	res.Write([]byte("/hash"))
+	pw := req.FormValue("password") // queue to storage
+
+	// TODO: move to service
+	pr := PasswordRow{
+		id: queue.Len() + 1, value: pw, createdAt: time.Now().Unix(),
+	}
+
+	queue.PushBack(pr)
+	repo[pr.id] = "..." // store empty string until hash is complete
+
+	fmt.Println(repo)
+
+	res.Write([]byte(strconv.Itoa(pr.id)))
 }
 
 func GetHashById(res http.ResponseWriter, req *http.Request) {
@@ -38,10 +78,9 @@ func GetHashById(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	hashId := strings.Split(path, "/")[2]
+	hashId, _ := strconv.Atoi(strings.Split(path, "/")[2])
 
-	fmt.Println(hashId)
-	res.Write([]byte("/hash/" + hashId))
+	res.Write([]byte(repo[hashId]))
 }
 
 func GetStats(res http.ResponseWriter, req *http.Request) {
@@ -50,7 +89,13 @@ func GetStats(res http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	res.Write([]byte("/stats"))
+	json, e := json.Marshal(Stats{total: 20, average: 2})
+
+	if e != nil {
+		log.Fatal(e)
+	}
+
+	res.Write([]byte(json))
 }
 
 func PostShutdown(res http.ResponseWriter, req *http.Request) {
